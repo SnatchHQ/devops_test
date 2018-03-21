cb547e67aa0fb1f8f69521295a18facc6ad776ea
Alessio Lunghi <alessiolunghi77@gmail.com>
2018-03-21 12:29:32 +0000

    Add test site.

---
+++ site/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to SnatchHQ</title>
+    <meta name="description" content="Download free Bootstrap 4 product landing page template Comply." />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/cropped-favicon-32x32.jpg" />
+    <!--Bootstrap 4-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <!--main section-->
+    <section class="bg-texture hero" id="main">
+      <div class="container">
+        <div class="row d-md-flex brand">
+            <div class="col-md-6 hidden-sm-down wow fadeIn">
+                <img class="img-fluid mx-auto d-block" src="img/snatch-logo.png"/>
+            </div>
+            <div class="col-md-6 col-sm-12 text-white wow fadeIn">
+              <h2 class="pt-4">Well done! <b class="text-primary-light">It's working :-) </b> </h2>
+              <p class="mt-5"><?php echo "This webpage is being served by php.";?></p>
+            </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
